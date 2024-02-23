47c85ab9d528c7f0d8a50cb5b479cef62687985d
Mike Smithson <mikesmithso@gmail.com>
2024-02-22 23:43:17 -0500

    use MockMvc with json matchers

--- src/test/java/com/smitty/spring/guru/controller/BeerControllerTest.java
+++ src/test/java/com/smitty/spring/guru/controller/BeerControllerTest.java
@@ -1,41 +1,53 @@
 package com.smitty.spring.guru.controller;
 
 import com.smitty.spring.guru.spring6restmvc.controller.BeerController;
+import com.smitty.spring.guru.spring6restmvc.model.Beer;
 import com.smitty.spring.guru.spring6restmvc.service.BeerService;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
+import static java.lang.String.join;
+import static org.hamcrest.Matchers.closeTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class BeerControllerTest {
     private MockMvc mockMvc;
+    private Beer testBeer;
+    private UUID randomUID;
 
-    @Mock
-    BeerService beerService;
 
     @BeforeEach
     void setUp() {
+        BeerService beerService = new BeerService();
+        testBeer = beerService.listBeers().get(0);
+        randomUID = testBeer.getId();
         mockMvc = MockMvcBuilders.standaloneSetup(new BeerController(beerService)).build();
     }
+
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beers/beer/" + UUID.randomUUID())
-                        .accept(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk());
+        mockMvc.perform(get(join("","/api/v1/beers/beer/", randomUID.toString()))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(randomUID.toString())))
+                .andExpect(jsonPath("$.version", is(testBeer.getVersion())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())))
+                .andExpect(jsonPath("$.beerStyle", is(testBeer.getBeerStyle().name())))
+                .andExpect(jsonPath("$.upc", is(testBeer.getUpc())))
+                .andExpect(jsonPath("$.quantityOnHand", is(testBeer.getQuantityOnHand())))
+                .andExpect(jsonPath("$.price", closeTo(testBeer.getPrice().doubleValue(), .0001)));
     }
 
 }
